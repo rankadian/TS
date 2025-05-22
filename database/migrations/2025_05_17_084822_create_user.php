6f7bb1829361983e955dd2a72d7e58599eeaddec
Amilil <amililuing86@gmail.com>
2025-05-22 12:49:14 +0700

    Add and Update Migration

--- database/migrations/2025_05_17_084822_create_user.php
+++ database/migrations/2025_05_17_084822_create_user.php
@@ -4,15 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('user_id');
+
+            // FK ke level
+            $table->unsignedBigInteger('level_id');
+            $table->foreign('level_id')->references('level_id')->on('level')->onDelete('cascade');
+
+            $table->string('google_id');
+            $table->string('google_token');
+            $table->string('google_refresh_token')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -22,9 +26,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
