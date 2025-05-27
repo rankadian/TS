539a333325ca2687c4483ddc4b089e4322581e0e
rankadian <dianthafafian@gmail.com>
2025-05-27 09:39:32 +0700

    add layout for footer, header, and breadcrumb

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Malang State Polytechnic</b>
+    </div>
+    <strong>Copyright &copy; 2025 <a href="https://adminlte.io">TSPM</a>.</strong> All rights reserved.
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
